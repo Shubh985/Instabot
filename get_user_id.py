73ad0140f55896becbf1b761ef6e296647739488
shubham985 <shubhamchauhan322@gmail.com>
2017-07-10 20:19:14 +0530

    first commit

---
+++ get_user_id.py
@@ -0,0 +1,21 @@
+#import file from other file
+import  requests
+import urllib
+from constant import *
+
+#get_user_id function
+def get_user_id(insta_username):
+  request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+  print 'GET request url : %s' %(request_url)
+  user_info=requests.get(request_url).json()
+  if user_info['meta']['code'] == 200:
+      if len(user_info['data']):
+          print user_info['data'][0]['id']
+      else:
+          print "user not exist"
+  else:
+      print "unable to acces the info"
+      exit()
+
+
+
